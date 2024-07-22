41faaed6edd34fe2e8c3b141f8485109615fa758
Deepanshu <deepanshu.9dd@gmail.com>
2024-07-22 23:38:22 +0530

    add comments

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+// middleware to allow access to only limited users
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
+
+//common middlewares
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
